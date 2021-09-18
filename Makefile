b2e61c3a9c3fdbe910c7b8670f247f3ff8105838
Sebastian Twarog <sebastian.twarog@efficioconsulting.com>
2021-09-18 14:30:28 +0200

    Installed app

---
+++ Makefile
@@ -0,0 +1,5 @@
+start:
+	php artisan serve
+
+behat:
+	vendor/bin/behat
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,50 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/', function (Request $r) {
+    $podatekDochodowy = 0.2;
+    $kwotaWolnaOdPodatku = 5000;
+    $dodatekVotujacy = 1000;
+    $wysokiPodatek = 100000;
+
+    $metoda = $r->post('metoda');
+    $dochod = (float)$r->post('dochod');
+
+    if (!is_numeric($r->post('dochod')) || $dochod < 0) {
+        throw new Exception('Zly dochód');
+    }
+
+    $tax = 0.0;
+
+    if ($metoda === 'one') {
+        $kwotaDoOpodatkowania = $dochod;
+    }
+
+    if ($metoda === 'two') {
+        $kwotaDoOpodatkowania = $dochod - $kwotaWolnaOdPodatku;
+
+        if ($kwotaDoOpodatkowania < 0) {
+            $kwotaDoOpodatkowania = 0;
+        }
+
+        if ($dochod >= $wysokiPodatek) {
+            $podatekDochodowy *= 2;
+        }
+    }
+
+    if ($kwotaDoOpodatkowania <= 0) {
+        if ($dochod <= 2000) {
+            $tax -= $dodatekVotujacy;
+        }
+    } else {
+        $tax = $podatekDochodowy * $kwotaDoOpodatkowania;
+    }
+
+    return view('welcome', [
+        'tax' => $tax,
+        'metoda' => $metoda,
+        'dochod' => $dochod
+    ]);
 });
