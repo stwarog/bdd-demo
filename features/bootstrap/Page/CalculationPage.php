1522ba7527d46e8cd369d1378c67398dcedb4f1f
Sebastian Twaróg <contact@stwarog.com>
2021-09-30 17:27:20 +0200

    Step 5: Refactored CalculationPage.php

--- features/bootstrap/Page/CalculationPage.php
+++ features/bootstrap/Page/CalculationPage.php
@@ -7,32 +7,37 @@ use SensioLabs\Behat\PageObjectExtension\PageObject\Page;
 
 class CalculationPage extends Page
 {
+    const INPUT_INCOME = '[name="dochod"]';
+    const INPUT_METHOD = '#metoda';
+    const BUTTON_SUBMIT = 'button';
+    const BOX_TAX_TO_PAY = '#tax';
+
     protected $path = '/';
 
     public function setIncomeValue($income)
     {
-        $element = $this->find('css', '[name="dochod"]');
+        $element = $this->find('css', self::INPUT_INCOME);
         $element->setValue($income);
     }
 
     public function selectMethod($method)
     {
-        $element = $this->find('css', '#metoda');
+        $element = $this->find('css', self::INPUT_METHOD);
         $element->setValue($method);
     }
 
     public function submitCalculation()
     {
-        $element = $this->find('css', 'button');
+        $element = $this->find('css', self::BUTTON_SUBMIT);
         $element->click();
     }
 
     public function getCalculatedTax()
     {
-        $element = $this->find('css', '#tax');
+        $element = $this->find('css', self::BOX_TAX_TO_PAY);
 
         if (empty($element)) {
-            throw new ElementNotFoundException(sprintf('Could not find an element "%s"', '#tax'));
+            throw new ElementNotFoundException(sprintf('Could not find an element "%s"', self::BOX_TAX_TO_PAY));
         }
 
         return $element->getHtml();
